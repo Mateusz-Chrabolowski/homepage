90b571aeaa8e5cd614cb293361e29dee8ce4662a
Mateusz Chrabolowski <mateusz.chrabolowski@gmail.com>
2025-11-23 03:10:13 +0100

    create a dark mode but already deleted

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-console.log("Witam szanownych szefunciów");
+{
+    const getElements = () => ({
+        removeButton: document.querySelector(".header__remove-btn"),
+        headerTitle: document.querySelector(".header__title"),
+    });
 
-let button = document.querySelector(".header__remove-btn");
-let headerTitle = document.querySelector(".header__title");
+    const handleHeaderRemove = (button, headerTitle) => {
+        if (headerTitle) {
+            headerTitle.remove();
+            button.textContent = "Nagłówek został usunięty";
+        } else {
+            button.textContent = "Nie ma już co usuwać";
+        }
+    };
 
-button.addEventListener("click", () => {
+    const addEvents = ({ removeButton, headerTitle }) => {
+        removeButton.addEventListener("click", () => {
+            handleHeaderRemove(removeButton, headerTitle);
+        });
+    };
 
-    if (headerTitle) {
-        headerTitle.remove();
-        headerTitle = null;
+    const init = () => {
+        const elements = getElements();
+        addEvents(elements);
+    };
 
-        button.textContent = "Nagłówek został usunięty";
-    } else {
-        button.textContent = "Nie ma już co usuwać";
-    }
-});
+    init();
+}
+
